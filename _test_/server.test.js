7f8e953a1e5d786ced95241fe478e0eeee7ed8a6
dimple23 <dimplechavda23@gmail.com>
2021-03-19 01:05:09 -0400

    update

--- _test_/server.test.js
+++ _test_/server.test.js
@@ -1,6 +1,14 @@
-require('regenerator-runtime/runtime')
-test('Testing the server', async () => {
-    const app = await require('../src/server/server.js')
-    //const res = await request(app)
-    expect(app).toBeDefined()
+const request = require('supertest')
+const app = require('../src/server/server');
+describe('Post Endpoints', () => {
+  it('should create a new post', async () => {
+    const res = await request(app)
+      .post('/add123')
+      .send({
+        userId: 1,
+        title: 'test is cool',
+      })
+    expect(res.statusCode).toEqual(201)
+    expect(res.body).toHaveProperty('post')
+  })
 })
